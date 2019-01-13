55d7a18f8c5cc52f19090240a3b608af9fe3be1a
Andy Lindsay <andydlindsay@gmail.com>
2019-01-13 11:56:38 -0800

    initial commit

---
+++ problem_1/p1.js
@@ -0,0 +1,10 @@
+// Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+// For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+module.exports = (arr, k) => {
+    // test if current value plus stored value equals to k
+    let returnValue = false;
+
+
+    return returnValue;
+};
---
+++ problem_1/test/test.js
@@ -0,0 +1,30 @@
+const { assert } = require('chai');
+const solution = require('../p1');
+
+describe('Tests for Problem #1', () => {
+
+  it('returns true given [10, 15, 3, 7] and k of 17', () => {
+    const arr = [10, 15, 3, 7];
+    const k = 17;
+    assert.equal(solution(arr, k), true);
+  });
+
+  it('returns false given [10, 15, 3, 7] and k of 14', () => {
+    const arr = [10, 15, 3, 7];
+    const k = 17;
+    assert.equal(solution(arr, k), false);
+  });
+
+  it('returns false given [] and k of 0', () => {
+    const arr = [];
+    const k = 0;
+    assert.equal(solution(arr, k), false);
+  });
+
+  it('returns true given [10, 15, 3, 7] and k of 18', () => {
+    const arr = [10, 15, 3, 7];
+    const k = 18;
+    assert.equal(solution(arr, k), true);
+  });
+
+});
