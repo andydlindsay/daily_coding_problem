a68ea56f20e50e604473abfbda23ad2ed0489aaf
Andy Lindsay <andydlindsay@gmail.com>
2019-01-13 12:04:16 -0800

    adds problem 1 solution

--- problem_1/p1.js
+++ problem_1/p1.js
@@ -2,9 +2,12 @@
 // For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
 module.exports = (arr, k) => {
-    // test if current value plus stored value equals to k
     let returnValue = false;
-
-
+    arr.forEach((elem, index) => {
+        const diff = k - elem;
+        if (arr.includes(diff) && arr.indexOf(diff) !== index) {
+            returnValue = true;
+        }
+    });
     return returnValue;
 };
--- problem_1/test/test.js
+++ problem_1/test/test.js
@@ -3,28 +3,28 @@ const solution = require('../p1');
 
 describe('Tests for Problem #1', () => {
 
-  it('returns true given [10, 15, 3, 7] and k of 17', () => {
-    const arr = [10, 15, 3, 7];
-    const k = 17;
-    assert.equal(solution(arr, k), true);
-  });
+    it('returns true given [10, 15, 3, 7] and k of 17', () => {
+        const arr = [10, 15, 3, 7];
+        const k = 17;
+        assert.equal(solution(arr, k), true);
+    });
 
-  it('returns false given [10, 15, 3, 7] and k of 14', () => {
-    const arr = [10, 15, 3, 7];
-    const k = 17;
-    assert.equal(solution(arr, k), false);
-  });
+    it('returns false given [10, 15, 3, 7] and k of 14', () => {
+        const arr = [10, 15, 3, 7];
+        const k = 14;
+        assert.equal(solution(arr, k), false);
+    });
 
-  it('returns false given [] and k of 0', () => {
-    const arr = [];
-    const k = 0;
-    assert.equal(solution(arr, k), false);
-  });
+    it('returns false given [] and k of 0', () => {
+        const arr = [];
+        const k = 0;
+        assert.equal(solution(arr, k), false);
+    });
 
-  it('returns true given [10, 15, 3, 7] and k of 18', () => {
-    const arr = [10, 15, 3, 7];
-    const k = 18;
-    assert.equal(solution(arr, k), true);
-  });
+    it('returns true given [10, 15, 3, 7] and k of 18', () => {
+        const arr = [10, 15, 3, 7];
+        const k = 18;
+        assert.equal(solution(arr, k), true);
+    });
 
 });
